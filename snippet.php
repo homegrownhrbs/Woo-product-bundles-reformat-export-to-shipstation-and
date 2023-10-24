89d5ab2e84f52b23148f9b7889a49192fecfb180
homegrownhrbs <92547255+homegrownhrbs@users.noreply.github.com>
2023-10-24 11:18:02 -0600

    Update snippet.php

--- snippet.php
+++ snippet.php
@@ -3,15 +3,13 @@
  * NOTE: 'shipstation_bundle_data' is a placeholder and should be replaced 
  * with the actual filter provided by the ShipStation plugin or WooCommerce 
  * if it's different.
- */
-add_filter('shipstation_bundle_data', 'modify_bundle_data_for_shipstation');
-
-/**
  * Modify the bundle data string by adding a line break after each product's SKU.
  *
  * @param string $bundle_data Original product bundle data string.
  * @return string Modified product bundle data string.
  */
+add_filter('shipstation_bundle_data', 'modify_bundle_data_for_shipstation');
+
 function modify_bundle_data_for_shipstation($bundle_data) {
     // Use regex to find the pattern "SKU - [any combination of characters/numbers]-[more characters/numbers],"
     $formatted_data = preg_replace('/(SKU - [^,]+),/', '$1,<br />', $bundle_data);
